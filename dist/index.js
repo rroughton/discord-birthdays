be7f8a0b7dd5f569ca04c8023bd917b45032a61d
Becca <roughton.rebecca@gmail.com>
2022-08-06 01:03:41 -0400

    initial setup

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const test = 'test';
+const local = false;
+const another = 3;
+const hello = 'hello0';
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const test: string = 'test';
+const local: boolean = false;
+const another: number = 3;
+const hello: string = 'hello0';
